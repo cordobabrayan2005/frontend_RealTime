6b7ef33d16116750be631bd52845e8e7e241ad5a
bryan_cr <cordoba.brayan@correounivalle.edu.co>
2025-12-14 15:05:11 -0500

    modularize

--- src/components/videocall/Toast.tsx
+++ src/components/videocall/Toast.tsx
@@ -1,14 +1,1 @@
-import React from 'react';
-
-interface ToastProps {
-  message: string;
-  isVisible: boolean;
-}
-
-export const Toast: React.FC<ToastProps> = ({ message, isVisible }) => {
-  if (!isVisible) {
-    return null;
-  }
-
-  return <div className="rt-toast show">{message}</div>;
-};
+export { Toast } from '../../features/videocall/components/Toast';
